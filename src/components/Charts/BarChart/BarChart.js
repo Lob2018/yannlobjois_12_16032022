7adbfab95b758dd0e4834cd30bc06a2ebabbbc6f
hatclic <@>
2022-04-21 16:45:24 +0200

    Feat : BarChart start of the tooltip

--- src/components/Charts/BarChart/BarChart.js
+++ src/components/Charts/BarChart/BarChart.js
@@ -102,6 +102,29 @@ const BarChart = ({ data = [], dimensions = {} }) => {
     const svgEl = d3.select(svgRef.current)
     svgEl.selectAll('*').remove() // Clear svg content before adding new elements
 
+    // Draws the background darkest color red for the selected value with opacity at 0
+    svgEl
+      .append('path')
+      .attr('d', function (d) {
+        return svgRoundedRectanglePath(
+          0,
+          0,
+          chartWidth - chartMarginLeft - chartMarginRight,
+          chartHeight - chartMarginBottom - chartMarginTop,
+          0,
+          {
+            tl: 0,
+            tr: 0,
+            br: 0,
+            bl: 0,
+          }
+        )
+      })
+      .attr('transform', `translate(-${chartMarginLeft},${chartMarginTop})`)
+      .attr('id', 'background-activity-day-selected-change')
+      .style('fill', '#C4C4C4')
+      .attr('opacity', 0)
+
     // the legends's svg
     const svgLegend = svgEl
       .append('svg')
@@ -282,8 +305,157 @@ const BarChart = ({ data = [], dimensions = {} }) => {
       .call((g) => g.select('.domain').attr('stroke', '#DEDEDE'))
       .call((g) => g.selectAll('.tick line').attr('y2', 0))
       .call((g) => g.selectAll('.tick text').attr('opacity', 0))
-  }, [data, height, width]) // Redraw chart if data or size changes
 
+    /**
+     * TOOLTIP
+     */
+    // Draws the tooltip and a mouse listener
+    var tooltip = addTooltip()
+    var bisectDate = d3.bisector((d) => d.day).center
+    svgChart
+      .append('rect')
+      .attr('opacity', '0')
+      .attr('width', width)
+      .attr('height', height)
+      .on('mouseover', function (event) {
+        tooltip.style('display', 'block')
+      })
+      .on('mouseout', function (event) {
+        tooltip.style('display', 'none')
+        // Hide the background
+        d3.select('#background-activity-day-selected-change').attr('opacity', 0)
+      })
+      .on('mousemove', mousemove)
+
+    /**
+     * Create the tooltip (with the circle for the value position)
+     * @function
+     * @memberof LineChart
+     * @returns {object} - The svg of the tooltip
+     */
+    function addTooltip() {
+      // Creating a group that will contain the entire tooltip plus the tracking circle
+      var tooltipGroup = svgChart
+        .append('g')
+        .attr('id', 'tooltip')
+        .style('display', 'none')
+      // Draw tooltip's background
+      tooltipGroup
+        .append('rect')
+        .attr('width', 39)
+        .attr('height', 25)
+        .style('fill', '#FFF')
+        .style('stroke', '#FFF')
+        .style('stroke-width', '1')
+        .attr('transform', 'translate(-19.5, 0)')
+        .attr('id', 'tooltip-container-weight-calorie')
+      // Draw the text container
+      var text = tooltipGroup
+        .append('text')
+        .attr('font-size', '8px')
+        .attr('font-weight', '500')
+        .style('color', '#000')
+        .attr('text-anchor', 'middle')
+        .style('fill', '#000')
+        .attr('transform', 'translate(0, 0)')
+        .attr('id', 'tooltip-text-container-weight-calorie')
+      // Draw the text for the average session value found
+      text
+        .append('tspan')
+        .attr('id', 'tooltip-text-weight-calorie')
+        .style('font-weight', 'bold')
+      return tooltipGroup
+    }
+
+    /**
+     * Update the tooltip text position for the first and the last values
+     * @function
+     * @memberof LineChart
+     * @param {number} i - The x axis day (0-6)
+     * @param {string} sessionLengthValue -The updated session's length value
+     */
+    function updateTooltipValue(i, sessionLengthValue) {
+      d3.select('#tooltip-text-weight-calorie').text(
+        sessionLengthValue + ' min'
+      )
+      if (i === 0) {
+        d3.select('#tooltip-container-weight-calorie').attr(
+          'transform',
+          'translate(19.5, -43)'
+        )
+        d3.select('#tooltip-text-container-weight-calorie').attr(
+          'transform',
+          'translate(39, -28)'
+        )
+      } else if (i === 6) {
+        d3.select('#tooltip-container-weight-calorie').attr(
+          'transform',
+          'translate(-58.5, -43)'
+        )
+        d3.select('#tooltip-text-container-weight-calorie').attr(
+          'transform',
+          'translate(-39, -28)'
+        )
+      } else {
+        d3.select('#tooltip-container-weight-calorie').attr(
+          'transform',
+          'translate(-19.5, -43)'
+        )
+        d3.select('#tooltip-text-container-weight-calorie').attr(
+          'transform',
+          'translate(0, -28)'
+        )
+      }
+    }
+
+    /**
+     * Update the darkest colorred background's width with the selected day
+     * @function
+     * @memberof LineChart
+     * @param {number} x - The x current position in pixels for the day selected
+     */
+    function updateDarkestBackgroundWidth(x) {
+      const bgPath = d3.select('#background-activity-day-selected-change')
+      bgPath
+        .attr('d', function () {
+          return svgRoundedRectanglePath(
+            x + chartWidth / 24 / 2,
+            chartMarginTop,
+            chartWidth / 12,
+            chartHeight,
+            0,
+            {
+              tl: 0,
+              tr: 0,
+              br: 0,
+              bl: 0,
+            }
+          )
+        })
+        .attr('opacity', x > chartWidth ? 0 : 1)
+        .merge(bgPath)
+    }
+
+    /**
+     * The mouse's listener to position the tooltip with the darkest background
+     * @function
+     * @memberof LineChart
+     * @param {object} event - The mouse event
+     */
+    function mousemove(event) {
+      // Tooltip
+      var x0 = xScaleText.invert(d3.pointer(event)[0]),
+        i = bisectDate(data[0].items, x0),
+        d = data[0].items[i]
+      const x = xScaleText(d.day)
+      //   const y = yScale(d.day)
+
+      tooltip.attr('transform', 'translate(' + x + ',' + chartMarginTop + ')')
+      updateTooltipValue(i, d.kilogram + ' ' + d.calories)
+      // Darkest background
+      updateDarkestBackgroundWidth(x)
+    }
+  }, [data, height, width]) // Redraw chart if data or size changes
   return <svg ref={svgRef} width={width} height={height} />
 }
 
